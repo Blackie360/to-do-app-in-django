b4825d48b6e6f2188eb1ce7e9dc0f3776d23cde7
Felix Jumason <felixkent360@gmail.com>
2023-02-06 18:13:48 +0300

    Initial commit

---
+++ base/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True,blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+def __string__(self):
+    return self.title
+
+class meta:
+    ordering = ['complete']
+
